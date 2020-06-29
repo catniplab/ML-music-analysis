experiments are running but something is very wrong with loss and accuracy computation

results/_sources/experiment_3790df84e393e14c4818f5b962bc8fe3.py
@@ -170,9 +170,8 @@ def log_accuracy(model: nn.Module,
         #_log.warning(str(prediction.shape))
         #_log.warning(str(target.shape))
 
-        # count total true positives for each sequence
-        true_pos = torch.sum(prediction*target, dim=2) # sum over channels (notes)
-        true_pos = torch.sum(true_pos, dim=1) # sum over time
+        # count total true positives
+        true_pos += torch.sum(prediction*target)
 
         # where we store accuracy at each time step
         acc_over_time = []
@@ -180,15 +179,10 @@ def log_accuracy(model: nn.Module,
         # see Bay et al 2009 for the definition of frame-level accuracy
         for t in range(T):
 
-            # get false positives and negatives for each sequence
-            false_pos = torch.sum(prediction[:, t]*(1 - target[:, t]), dim=1)
-            false_neg = torch.sum((1 - prediction[:, t])*target[:, t], dim=1)
+            false_pos = torch.sum(prediction[:, t]*(1 - target[:, t]))
+            false_neg = torch.sum((1 - prediction[:, t])*target[:, t])
 
-            # compute accuracy for each sequence
-            acc_each_sequence = true_pos/(true_pos + false_pos + false_neg)
-
-            # total across all sequences in the batch
-            acc_over_time.append(torch.sum(acc_each_sequence))
+            acc_over_time.append(true_pos/(true_pos + false_neg + false_neg))
 
         # return average over time
         return np.mean(acc_over_time)

results/_sources/experiment_61d62ac055117c09e9cb1fc7103ef7ba.py
@@ -70,7 +70,8 @@ def cfg():
                   'input_size': 88,
                   'hidden_size': 300,
                   'num_layers': 1,
-                  'output_size': 88
+                  'output_size': 88,
+                  'lin_readout': True
                  }
 
     # supported initializations
@@ -134,7 +135,7 @@ def log_loss(loss_fcn: nn.Module,
         output_tensor = torch.cat(output).reshape(T, this_batch, 88).permute([1, 0, 2])
 
         # append average loss over time
-        loss = loss_fcn(output_tensor, target_tensor)
+        loss = loss_fcn(output_tensor, target_tensor)/T
         all_loss.append(loss.cpu().detach().item())
 
     # log the mean across every batch
@@ -155,11 +156,7 @@ def log_accuracy(model: nn.Module,
     :return: average accuracy for every batch in the loader
     """
 
-    # store weighted accuracy for each batch
     all_acc = []
-
-    # count total number of sequences
-    num_seqs = 0
 
     def acc_fcn(output, target):
 
@@ -170,9 +167,10 @@ def log_accuracy(model: nn.Module,
         #_log.warning(str(prediction.shape))
         #_log.warning(str(target.shape))
 
-        # count total true positives for each sequence
-        true_pos = torch.sum(prediction*target, dim=2) # sum over channels (notes)
-        true_pos = torch.sum(true_pos, dim=1) # sum over time
+        # count total true positives
+        true_pos = 0
+        for t in range(T):
+            true_pos += torch.sum(prediction[:, t]*target[:, t])
 
         # where we store accuracy at each time step
         acc_over_time = []
@@ -180,15 +178,10 @@ def log_accuracy(model: nn.Module,
         # see Bay et al 2009 for the definition of frame-level accuracy
         for t in range(T):
 
-            # get false positives and negatives for each sequence
-            false_pos = torch.sum(prediction[:, t]*(1 - target[:, t]), dim=1)
-            false_neg = torch.sum((1 - prediction[:, t])*target[:, t], dim=1)
+            false_pos = torch.sum(prediction[:, t]*(1 - target[:, t]))
+            false_neg = torch.sum((1 - prediction[:, t])*target[:, t])
 
-            # compute accuracy for each sequence
-            acc_each_sequence = true_pos/(true_pos + false_pos + false_neg)
-
-            # total across all sequences in the batch
-            acc_over_time.append(torch.sum(acc_each_sequence))
+            acc_over_time.append(true_pos/(true_pos + false_neg + false_neg))
 
         # return average over time
         return np.mean(acc_over_time)
@@ -197,17 +190,15 @@ def log_accuracy(model: nn.Module,
 
         this_batch = target_tensor.shape[0]
         T = target_tensor.shape[1]
-        num_seqs += this_batch
 
         output, hiddens = model(input_tensor)
         output_tensor = torch.cat(output).reshape(T, this_batch, 88).permute([1, 0, 2])
 
-        # append weighted accuracy to the accumulation list
         acc = acc_fcn(output_tensor.cpu(), target_tensor.cpu())
         all_acc.append(acc)
 
     # log the average accuracy across every batch
-    _run.log_scalar(log_name, np.sum(all_acc)/num_seqs)
+    _run.log_scalar(log_name, np.mean(all_acc))
 
 
 # main function

results/_sources/models_3cc6e93ec46bb2b13d715339551cd5f6.py
@@ -18,7 +18,7 @@ from torch.distributions.distribution import Distribution
 
 from math import sin, cos
 
-from src.base_models import LINEAR_CUDA, LINEAR_JIT, REGRESSION
+from src.base_models import LINEAR_CUDA, LINEAR_JIT, REGRESSION_CUDA, REGRESSION_JIT
 
 # TODO
 # make sure we are using the same conventions as pytorch RNNs in terms of storing hidden states
@@ -150,10 +150,13 @@ def get_model(model_dict: dict, initializer: dict, cuda: bool) -> ReadOutModel:
 
     # construct the model
     model = None
-    if model_dict['architecture'] != "REGRESSION":
+    if model_dict['lin_readout']:
         model = ReadOutModel(model_dict, cuda)
     else:
-        model = REGRESSION()
+        if cuda:
+            model = REGRESSION_CUDA(model_dict)
+        else:
+            model = REGRESSION_JIT(model_dict)
 
     # initialize the model
     if initializer['init'] != 'default':

src/base_models.py
@@ -82,11 +82,11 @@ class LINEAR_CUDA(nn.Module):
 
 
 # Simple affine transformation of the last time step, doesn't take the past into account
-class REGRESSION_CUDA(nn.Module):
+class REGRESSION(nn.Module):
 
-    def __init__(self, model_dict):
+    def __init__(self):
 
-        super(REGRESSION_CUDA, self).__init__()
+        super(REGRESSION, self).__init__()
 
         self.mt19937 = np.random.MT19937()
         self.hh_seed = np.random.get_state()
@@ -95,32 +95,11 @@ class REGRESSION_CUDA(nn.Module):
 
     def forward(self, x):
 
-        device = self.weights.weight.data.get_device()
+        device = 'cpu'
+        ix = self.weights.weight.data.get_device()
+        if ix > -1:
+            device = ix
         x = x.to(device)
-
-        N = x.shape[0]
-        T = x.shape[1]
-
-        outputs = []
-        for t in range(T):
-            outputs.append(self.weights(x[:, t]))
-
-        return outputs, outputs
-
-
-# Simple affine transformation of the last time step, doesn't take the past into account
-class REGRESSION_JIT(nn.Module):
-
-    def __init__(self, model_dict):
-
-        super(REGRESSION_JIT, self).__init__()
-
-        self.mt19937 = np.random.MT19937()
-        self.hh_seed = np.random.get_state()
-
-        self.weights = nn.Linear(88, 88)
-
-    def forward(self, x):
 
         N = x.shape[0]
         T = x.shape[1]