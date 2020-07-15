c25e4d5d77c441fdef01b46e445b60c577bef462
Eben Kadile <eben.kadile24@gmail.com>
2020-07-15 01:27:25 -0700

    sklearn results

--- sklearn-stuff/trainer.py
+++ sklearn-stuff/trainer.py
@@ -38,8 +38,43 @@ for i in range(49):
     this_model.fit(X, Y[:, i])
     model_list.append(this_model)
 
-def compute_accuracy():
-    raise NotImplementedError
+def compute_accuracy(set: str):
+
+    arrays = data_dict[set][0]
+
+    tot_over_seqs = 0
+
+    for array in arrays:
+
+        tot_over_time = 0
+
+        for t in range(len(array) - 1):
+
+            x = array[t, 27 : 76]
+            y = array[t + 1, 27 : 76]
+
+            tp = 0
+            fp = 0
+            fn = 0
+
+            for channel in range(49):
+
+                model = model_list[channel]
+                pred = model.predict(x.reshape(1, -1))[0]
+
+                tp += y[channel]*pred
+                fp += (1 - y[channel])*pred
+                fn += y[channel]*(1 - pred)
+
+            if tp == 0 and fp == 0 and fn == 0:
+                tot_over_time += 1
+            else:
+                tot_over_time += tp/(tp + fp + fn)
+
+        tot_over_seqs += tot_over_time/len(array)
+
+    return tot_over_seqs/len(arrays)
+
 
 def compute_loss():
     raise NotImplementedError
